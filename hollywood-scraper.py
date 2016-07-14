5e4933056ba946fcf8fcaec6fcf174788e38f4b0
Derek Miller <7derek3@gmail.com>
2016-07-14 00:59:14 -0700

    correctly format date and print the text of each film and time

--- hollywood-scraper.py
+++ hollywood-scraper.py
@@ -1,11 +1,19 @@
 from lxml import etree
 from io import StringIO, BytesIO
 import requests
+from datetime import datetime
 
-parser = etree.HTMLParser()
 page = requests.get('http://hollywoodtheatre.org/wp-admin/admin-ajax.php?action=aec_ajax&aec_type=widget&aec_widget_id=aec_widget-5-container&aec_month=7&aec_year=2016')
-events = tree.xpath('/html/body/table/tbody/tr[4]/td[4]/div')
+parser = etree.HTMLParser()
+tree = etree.parse(StringIO(page.text), parser)
+events = tree.xpath('//div[@class=\'aec-event-info\']')
 
-result = etree.tostring(tree.getroot(), pretty_print=True, method="html")
+for event in events:
+    date = event.xpath('h2[@class=\'widgettitle\']/text()')[0]
+    date = date.replace("Showing ", "")
+    date = datetime.strptime(date, '%B %d, %Y')
+    print(date.strftime('%B %d, %Y'))
+    films = event.xpath('.//li[@class=\'aec-tooltip-feed-agile\']')
 
-print(result)
+    for film in films:
+        print(film.xpath('p/strong/text()')[0])
