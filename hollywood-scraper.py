25980f0f81dbf0e029263ca2483caa1517371a64
Derek Miller <7derek3@gmail.com>
2016-07-21 11:53:37 -0700

    Put things in some proper functions and call from main

--- hollywood-scraper.py
+++ hollywood-scraper.py
@@ -6,22 +6,32 @@ from datetime import datetime
 
 def buildUri(month, year):
     baseUri = 'http://hollywoodtheatre.org/wp-admin/admin-ajax.php?action=aec_ajax&aec_type=widget&aec_widget_id=aec_widget-5-container'
-    monthUri = '&aec_month=' + str(month)
-    yearUri = '&aec_year=' + str(year)
-    uri = baseUri + monthUri + yearUri
+    monthArg = '&aec_month=' + str(month)
+    yearArg = '&aec_year=' + str(year)
+    uri = baseUri + monthArg + yearArg
     return uri
 
-page = requests.get(buildUri(7, 2016))
-parser = etree.HTMLParser()
-tree = etree.parse(StringIO(page.text), parser)
-days = tree.xpath('//div[@class=\'aec-event-info\']')
+def makeRequest(uri):
+    page = requests.get(uri)
+    parser = etree.HTMLParser()
+    tree = etree.parse(StringIO(page.text), parser)
+    days = tree.xpath('//div[@class=\'aec-event-info\']')
+    return days
 
-for day in days:
-    date = day.xpath('h2[@class=\'widgettitle\']/text()')[0]
-    date = date.replace("Showing ", "")
-    date = datetime.strptime(date, '%B %d, %Y')
-    print(date.strftime('%B %d, %Y'))
-    films = day.xpath('.//li[@class=\'aec-tooltip-feed-agile\']')
+def parseHtml(days):
+    for day in days:
+        date = day.xpath('h2[@class=\'widgettitle\']/text()')[0]
+        date = date.replace("Showing ", "")
+        date = datetime.strptime(date, '%B %d, %Y')
+        print(date.strftime('%B %d, %Y'))
+        films = day.xpath('.//li[@class=\'aec-tooltip-feed-agile\']')
 
-    for film in films:
-        print(film.xpath('p/strong/text()')[0])
+        for film in films:
+            print(film.xpath('p/strong/text()')[0])
+
+def main():
+    uri = buildUri(7, 2016)
+    days = makeRequest(uri)
+    output = parseHtml(days)
+
+main()
