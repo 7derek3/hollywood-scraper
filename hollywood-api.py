e34ebbe88b565c7207af54a41c839f3699608719
Derek Miller <7derek3@gmail.com>
2016-09-05 16:11:55 -0700

    make api response output in json

--- hollywood-api.py
+++ hollywood-api.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 import psycopg2
 
 app = Flask(__name__)
@@ -10,13 +11,14 @@ def show_all_dates():
 
     cur.execute('SELECT * FROM showings;')
     data = cur.fetchall()
-    showings = ''
+    showings = []
     for x in data:
+        _id = x[0]
         title = x[1]
         time = x[2]
         url = x[3]
-        showings += '{}, {}, {}'.format(title, time, url)
-    return showings
+        showings.append({'id': _id, 'title': title, 'time': time, 'url': url})
+    return jsonify(showings)
 
     cur.close()
     conn.close()
