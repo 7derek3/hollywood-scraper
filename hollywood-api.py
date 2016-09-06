0d34bb9b626008dd264d669127e9aeb6a92d1df6
Derek Miller <7derek3@gmail.com>
2016-09-05 17:35:27 -0700

    start adding in ability to select date ranges

--- hollywood-api.py
+++ hollywood-api.py
@@ -1,15 +1,26 @@
 from flask import Flask
 from flask import jsonify
 import psycopg2
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def show_all_dates():
+def get_showings(start_date='', end_date=''):
     conn = psycopg2.connect("dbname=hollywood user=derekmiller")
     cur = conn.cursor()
+    if start_date == '' and end_date == '':
+        start_date = datetime.datetime.today()
+        end_date = start_date + datetime.timedelta(days=7)
+        start_date = start_date.strftime('%Y-%m-%d')
+        end_date = end_date.strftime('%Y-%m-%d')
+    elif end_date == '':
+        end_date == start_date
 
-    cur.execute('SELECT * FROM showings;')
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+    cur.execute('SELECT * FROM showings WHERE time BETWEEN %s AND %s;', (start_date, end_date))
     data = cur.fetchall()
     showings = []
     for x in data:
